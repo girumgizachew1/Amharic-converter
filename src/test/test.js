62936b76a87ddb0fbef23799df2d9b25c34d5866
Eyob <eabebe91@gmail.com>
2023-05-30 19:47:21 +0300

    src folder created for max readability

--- src/test/test.js
+++ src/test/test.js
@@ -1,4 +1,4 @@
-const { convertToAmharic } = require('./index');
+const { convertToAmharic } = require('../index');
 //testing with a given english word
 const englishText = "hame";
 const amharicText = convertToAmharic(englishText);
