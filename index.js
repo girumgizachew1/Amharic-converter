419b421654ffcd8a965c80062db1aa747c267be8
girum <girumegizachew@gmail.com>
2023-06-05 06:28:27 -0400

    fixed some letter issue

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ function convertToAmharic(text) {
   let convertedText = text;
   // Apply the transcription rules to convert the text to Amharic
   for (const rule in transcriptionRules) {
-    const regex = new RegExp(rule, "gi");
+    const regex = new RegExp(rule, "g");
     convertedText = convertedText.replace(regex, transcriptionRules[rule]);
   }
 
   // Return the converted text
   return convertedText;
 }
-//exporting the funtion
+
+// Exporting the function
 module.exports = {
   convertToAmharic
 };
--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 const { convertToAmharic } = require('./index');
 //testing with a given english word
-const englishText = "hame";
-const amharicText = convertToAmharic(englishText);
+const englishText = 'he hu hi ha hee h ho hua , le lu li la lee l lo lua , He Hu Hi Ha Hee H Ho Hua';
+const englishText1 = 'Me mu mi ma mee m mo mua mY  , Se Su Si Sa See S So Sua , Re ru ri ra ree r ro rua rY ';
+const englishText2 = 'se su si sa see s so sua ';
+const englishText3 = 'a u i ea ee e o ae';
+
+const amharicText = convertToAmharic(englishText3);
 console.log(amharicText);
